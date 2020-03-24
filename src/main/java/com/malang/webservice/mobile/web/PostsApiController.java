94377597e208fef241f8bce2fde5669be90d462e
yonggyeom <winder0524@gmail.com>
2020-03-24 21:25:57 +0900

    fixed untracked files

---
+++ src/main/java/com/malang/webservice/mobile/web/PostsApiController.java
@@ -0,0 +1,37 @@
+package com.malang.webservice.mobile.web;
+
+import com.malang.webservice.mobile.service.PostsService;
+import com.malang.webservice.mobile.web.dto.PostsResponseDto;
+import com.malang.webservice.mobile.web.dto.PostsSaveRequestDto;
+import com.malang.webservice.mobile.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
+}
