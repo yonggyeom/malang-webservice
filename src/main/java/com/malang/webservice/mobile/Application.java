4e9f73668460506a00217318ecc9be89796fa46a
yonggyeom <winder0524@gmail.com>
2020-03-26 21:31:09 +0900

    AWS S3 다시 테스트(수정)

--- src/main/java/com/malang/webservice/mobile/Application.java
+++ src/main/java/com/malang/webservice/mobile/Application.java
@@ -7,21 +7,21 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class Application {
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.properties,"
-            + "classpath:application-oauth.properties,"
-            + "classpath:application-real.properties,"
-            + "classpath:application-real1.properties,"
-            + "classpath:application-real2.properties,"
-            + "classpath:aws.properties";
+//    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+//            + "classpath:application.properties,"
+//            + "classpath:application-oauth.properties,"
+//            + "classpath:application-real.properties,"
+//            + "classpath:application-real1.properties,"
+//            + "classpath:application-real2.properties,"
+//            + "classpath:aws.properties";
+//
+//    public static void main(String[] args) {
+//        new SpringApplicationBuilder(Application.class)
+//                .properties(APPLICATION_LOCATIONS)
+//                .run(args);
+//    }
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(Application.class, args);
     }
-
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
 }
