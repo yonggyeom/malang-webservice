208f29bf34b73f257e05d23437c22b2c1bf7fbec
yonggyeom <winder0524@gmail.com>
2020-03-29 16:03:06 +0900

    AWS S3 테스트

--- src/test/java/com/malang/webservice/mobile/web/PostsApiControllerTest.java
+++ src/test/java/com/malang/webservice/mobile/web/PostsApiControllerTest.java
@@ -86,37 +86,37 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
 
-//    @Test
-//    @WithMockUser(roles="USER")
-//    public void Posts_수정된다() throws Exception {
-//        //given
-//        Posts savedPosts = postsRepository.save(Posts.builder()
-//                .title("title")
-//                .content("content")
-//                .author("author")
-//                .build());
-//
-//        Long updateId = savedPosts.getId();
-//        String expectedTitle = "title2";
-//        String expectedContent = "content2";
-//
-//        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
-//                .title(expectedTitle)
-//                .content(expectedContent)
-//                .build();
-//
-//        String url = "http://localhost:" + port + "/api/v1/posts/update/" + updateId;
-//
-//        //when
-//        mvc.perform(put(url)
-//                .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                .content(new ObjectMapper().writeValueAsString(requestDto)))
-//                .andExpect(status().isOk());
-//
-//        //then
-//        List<Posts> all = postsRepository.findAll();
-//        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
-//        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
-//    }
+    @Test
+    @WithMockUser(roles="USER")
+    public void Posts_수정된다() throws Exception {
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/update/" + updateId;
+
+        //when
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+        //then
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+    }
 
 }
