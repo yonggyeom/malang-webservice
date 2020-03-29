81afac999ad8ce022d51be945f5fdefb3309f2ee
yonggyeom <winder0524@gmail.com>
2020-03-29 15:38:14 +0900

    AWS S3 테스트

--- src/main/java/com/malang/webservice/mobile/web/PostsApiController.java
+++ src/main/java/com/malang/webservice/mobile/web/PostsApiController.java
@@ -18,17 +18,17 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
-    @PutMapping("/api/v1/posts/{id}")
+    @PutMapping("/api/v1/posts/update/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
+    @GetMapping("/api/v1/posts/find/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
     }
 
-    @DeleteMapping("/api/v1/posts/{id}")
+    @DeleteMapping("/api/v1/posts/delete/{id}")
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
         return id;
