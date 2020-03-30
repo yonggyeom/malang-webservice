681cf0f044b99f4942a2cf8d857bb4437582b7cd
yonggyeom <winder0524@gmail.com>
2020-03-30 20:45:34 +0900

    AWS S3 - 이미지업로드 테스트(URL까지 POST 테이블에 저장)

--- src/main/java/com/malang/webservice/mobile/web/dto/PostsResponseDto.java
+++ src/main/java/com/malang/webservice/mobile/web/dto/PostsResponseDto.java
@@ -10,11 +10,13 @@ public class PostsResponseDto {
     private String title;
     private String content;
     private String author;
+    private String imageUrl;
 
     public PostsResponseDto(Posts entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
         this.author = entity.getAuthor();
+        this.imageUrl = entity.getImageUrl();
     }
 }
