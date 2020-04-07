522f561b4c8040c3a6ff03bc67a0c4ee14b99922
yonggyeom <winder0524@gmail.com>
2020-04-07 19:10:01 +0900

    WebSocket 구현

--- src/main/java/com/malang/webservice/mobile/config/WebSocketConfig.java
+++ src/main/java/com/malang/webservice/mobile/config/WebSocketConfig.java
@@ -2,10 +2,16 @@ package com.malang.webservice.mobile.config;
 
 import com.malang.webservice.mobile.handler.SocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableWebSocket
@@ -14,8 +20,19 @@ public class WebSocketConfig implements WebSocketConfigurer{
     @Autowired
     SocketHandler socketHandler;
 
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
+    }
+
+    // Of course , you can define the Executor too
+    // @Bean
+    // public Executor taskExecutor() {
+    //     return new SimpleAsyncTaskExecutor();
+    // }
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(socketHandler, "/chating");
+        registry.addHandler(socketHandler, "/chatting");
     }
 }
