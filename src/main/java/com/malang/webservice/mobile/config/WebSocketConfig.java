ca7361d0f1079c3e7cf25d5876d06906df1e618f
yonggyeom <winder0524@gmail.com>
2020-04-08 23:29:46 +0900

    WebSocket 구현_테스트중

--- src/main/java/com/malang/webservice/mobile/config/WebSocketConfig.java
+++ src/main/java/com/malang/webservice/mobile/config/WebSocketConfig.java
@@ -33,7 +33,47 @@ public class WebSocketConfig implements WebSocketConfigurer{
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//        registry.addHandler(socketHandler, "/chating");
-        registry.addHandler(socketHandler, "/chating/{roomNumber}");
+        registry.addHandler(socketHandler, "/chating");
     }
 }
+
+
+//package com.malang.webservice.mobile.config;
+//
+//import com.malang.webservice.mobile.handler.SocketHandler;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.task.SimpleAsyncTaskExecutor;
+//import org.springframework.scheduling.TaskScheduler;
+//import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+//import org.springframework.web.socket.config.annotation.EnableWebSocket;
+//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+//
+//import java.util.concurrent.Executor;
+//
+//@Configuration
+//@EnableWebSocket
+//public class WebSocketConfig implements WebSocketConfigurer{
+//
+//    @Autowired
+//    SocketHandler socketHandler;
+//
+//    @Bean
+//    public TaskScheduler taskScheduler() {
+//        return new ConcurrentTaskScheduler();
+//    }
+//
+//    // Of course , you can define the Executor too
+//    // @Bean
+//    // public Executor taskExecutor() {
+//    //     return new SimpleAsyncTaskExecutor();
+//    // }
+//
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+////        registry.addHandler(socketHandler, "/chating");
+//        registry.addHandler(socketHandler, "/chating/{roomNumber}");
+//    }
+//}
